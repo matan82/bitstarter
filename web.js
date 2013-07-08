95b70d150bfff035ee5ecbd804e3c78de7b0d2e6
matan82 <ma_bi_sa@hotmail.com>
2013-07-08 16:21:17 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var buffer = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
     
-    response.send(buffer.toString);
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
